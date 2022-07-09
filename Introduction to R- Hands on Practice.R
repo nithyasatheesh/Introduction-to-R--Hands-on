339078465090253de6937fd0a5d5f13607da5fd4
nithyasatheesh <32924400+nithyasatheesh@users.noreply.github.com>
2022-07-09 19:13:08 +0530

    Add files via upload

---
+++ Introduction to R- Hands on Practice.R
@@ -0,0 +1,42 @@
+### Hands-on practice in Data structures-R  ###
+
+##-------------- Vector-----------------------#
+
+#1. Create a vector 'V' with numeric values
+#2. Add each element in vector 'V' with 10.
+#3. Find the sum and mean of a vector 'v'.
+#4. Create empty vector 'New' and append the values "hello",45,21,"Hi"
+#5. Insert value 14.5 at last position in the vector 'New'
+#6. Extract the values 2.1 and 3.3 from the vector x<-c(2.1,4.2,3.3,5.4)
+
+###-------------------------------------------------------###
+###------------------Matrix------------------------------###
+
+# 1. Create a matrix y with 9 integers of no of rows is 3 and give colnames as A,B,C
+# Extract the values comes at rows 1 & 2 and columns 2 & 3 from the matrix y
+# 2. Multiply a matrix 'y' by the vector v=c(1,2,3)
+
+###-----------------------------------------------------##
+##------------- Array----------------------------------##
+# Create an array of 25 elements by using array()
+
+# Assign dim names by using dimnames()
+
+###-----------------------------------------------###
+##----------------List---------------------------###
+#1. Create a list 'newlist' from the given two vector objects p and q
+# p <- c(2,7,8),q <- c("A", "B", "C") 
+# And find the value of newlist[2]
+
+#2.Let x <- list(a=5:10, c="Hello", d="AA"), write an R statement 
+# to add a new item z = "NewItem" to the list x.
+
+##---------------DataFrame--------------#
+
+# Create a simple data frame from 3 vectors. 
+# Order the entire data frame by the first column.
+##-----------------------------------------------------------------##
+
+## Load/Read given csv file in R and and name it as 'oj'
+## Load/Read  given excel file in R and and name it as 'Insurance'
+## Load/Read  given json file in R and name it as 'data'
